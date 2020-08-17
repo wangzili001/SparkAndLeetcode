14868ac5b4272eb849ecf554c5815c0c39e87a04
wangzili001 <wangzili1995@qq.com>
2020-08-17 18:51:02 +0800

    新增乐扣 链表反转

---
+++ leetcode/src/main/java/leetcode/ReverseKGroup.java
@@ -0,0 +1,64 @@
+package leetcode;
+
+import lombok.Data;
+
+/**
+ * @author： Oliver
+ * @date： 2020/8/17
+ *
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ *
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ *
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给你这个链表：1->2->3->4->5
+ *
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ *
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ *
+ *  
+ *
+ * 说明：
+ *
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ */
+public class ReverseKGroup {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        while (listNode1.next != null){
+            System.out.println(listNode1.getVal());
+            listNode1 = listNode1.next;
+        }
+
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return head;
+    }
+}
+
+@Data
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+
